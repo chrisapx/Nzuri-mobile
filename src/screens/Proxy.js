5a302dfb1283ac5cba74a47b8dce62921f6c371e
Chris <96912309+Chris-Mwesigwa@users.noreply.github.com>
2023-07-09 21:12:09 +0300

    proxy page

---
+++ src/screens/Proxy.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Proxy = () => {
+    return(
+        <View>
+            <Text>This is the proxy page</Text>
+        </View>
+    )
+}
+
+export default Proxy;
